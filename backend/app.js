986cad75235adbd1aeb67d3b4276bfdcfcf62f8f
navalraul <129669942+navalraul@users.noreply.github.com>
2024-07-03 09:44:52 +0530

    nmj

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello  ecls from backend")
+    res.send("Hello  from backend")
 })
 
 app.listen(1000, () => {
